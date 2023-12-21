2e146265409393c936a97f0beb9e00137cbda94f
Stas-Steshenko <149504848+Stas-Steshenko@users.noreply.github.com>
2023-12-21 18:46:46 +0200

    Add files via upload

---
+++ Lab15_6/Lab15_6/Lab15_6.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <Windows.h>
+using namespace std;
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    int n;
+    cout << "Введіть розмір масиву: ";
+    cin >> n;
+    int* myArray = new int[n];
+
+    cout << "Введіть цілі елементи масиву: " << endl;
+    for (int i = 0; i < n; ++i) {
+        cout << "Елемент " << i + 1 << ": ";
+        cin >> myArray[i];
+    }
+    
+    int max = myArray[0];
+    int min = myArray[0];
+
+    for (int i = 1; i < n; ++i) {
+        if (myArray[i] > max) {
+            max = myArray[i];
+        }
+        if (myArray[i] < min) {
+            min = myArray[i];
+        }
+    }
+    cout << "Максимальне значення: " << max << endl;
+    cout << "Мінімальне значення: " << min << endl;
+    delete[] myArray;
+
+    return 0;
+}
---
+++ Lab15_7/Lab15_7/Lab15_7.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <windows.h>
+using namespace std;
+
+int main() {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    int s = 0;
+    int n;
+
+    cout << "Введіть кількість чисел: ";
+    cin >> n;
+
+    for (int i = 0; i < n; ++i) {
+        int num;
+        cout << "Введіть число " << i + 1 << ": ";
+        cin >> num;
+
+        s += num;
+    }
+    cout << "Накопичена сума: " << s << endl;
+
+    return 0;
+}
